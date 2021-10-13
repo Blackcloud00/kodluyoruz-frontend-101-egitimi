bfea558aca6018ff194cd2ef9dfb804107fd51ce
Newt0n <47007924+Blackcloud00@users.noreply.github.com>
2021-10-14 01:21:07 +0300

    SQL exercise 2

---
+++ SQL/odev-2.sql
@@ -0,0 +1,14 @@
+--SELECT * FROM public.film WHERE (replacement_cost BETWEEN 12.99 AND 16.99) ORDER BY film_id ASC 
+--SELECT first_name, last_name FROM public.actor WHERE first_name IN('Penelope','Nick','Ed') ORDER BY actor_id ASC
+--SELECT * FROM public.film WHERE rental_rate IN(0.99,2.99,4.99) AND replacement_cost IN(12.99,15.99,28.99) ORDER BY film_id ASC
+
+
+
+
+/*
+film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız 
+( BETWEEN - AND yapısını kullanınız.)
+.actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed'
+değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
+film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız.
+( IN operatörünü kullanınız.)*/
