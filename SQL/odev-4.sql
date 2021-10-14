8bc857227ccddfce93fa1e5a1f4626aae6d81934
Newt0n <47007924+Blackcloud00@users.noreply.github.com>
2021-10-14 21:52:08 +0300

    Odev-4 Sql sayfası

---
+++ SQL/odev-4.sql
@@ -0,0 +1,4 @@
+--SELECT DISTINCT replacement_cost FROM film;
+--SELECT COUNT( DISTINCT replacement_cost) FROM film;
+--SELECT COUNT(*) FROM film WHERE title LIKE 'A%' AND rating = 'G';
+SELECT COUNT(*) FROM country 
